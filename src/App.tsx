993481068eb27f65ffa8ef0db18da04059c2cc42
pematthews41 <pematthews41@gmail.com>
2025-02-21 13:49:55 -0500

    refactor: remove navigation menu and old dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { theme } from './theme';
-import Layout from './components/Layout';
 import ServiceMenu from './pages/ServiceMenu';
 import SeatingChart from './pages/SeatingChart';
-import AppRoutes from './routes';
-import Navigation from './components/Navigation';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
@@ -14,14 +11,11 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <Navigation />
           <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<ServiceMenu />} />
-              <Route path="/seating-chart" element={<SeatingChart />} />
-              {/* Add other routes as they are implemented */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Route>
+            <Route path="/" element={<ServiceMenu />} />
+            <Route path="/seating-chart" element={<SeatingChart />} />
+            {/* Add other routes as they are implemented */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
       </ThemeProvider>
